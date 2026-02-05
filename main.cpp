713f6882951e2008917bb15d41d945eb1d269b64
jsanchez-codes <julian.sanchez@bsu.edu>
2026-02-05 02:51:03 +0000

    Starter Horse Code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "horse.h"
+
+void testHorse();
 
 int main(){
 	std::cout << "OOP Horse Race!" << std::endl;
+
+	testHorse();
+
 	return 0;
-}
+} // end main
+
+void testHorse(){
+	Horse h;
+	h.init(1, 15);
+	h.printLane();
+} // end testHorse
+
 
--- makefile
+++ makefile
@@ -1,5 +1,11 @@
-horseRace: main.cpp
-	g++ -g main.cpp -o horseRace
+horseRace: main.o horse.o
+	g++ -g main.o horse.o -o horseRace
+
+main.o: main.cpp horse.h
+	g++ -g -c main.cpp
+
+horse.o: horse.h horse.cpp
+	g++ -g -c horse.cpp
 
 clean:
 	rm horseRace
